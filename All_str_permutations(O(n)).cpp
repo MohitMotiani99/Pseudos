3ea179bf1073c2f8068b08738f5d9de0d64ddf85
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2020-08-31 22:25:48 +0530

    Create All_str_permutations(O(n)).cpp

---
+++ All_str_permutations(O(n)).cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    do{
+        cout<<str<<endl;
+    }while(next_permutation(str.begin(),str.end()));
+    
+}
