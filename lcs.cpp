e87dd3200708fb593e7c607505bc80720d0b4430
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2020-09-03 22:15:50 +0530

    Create lcs.cpp

---
+++ lcs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str1,str2;
+    cin>>str1>>str2;
+    int m=str1.size();
+    int n=str2.size();
+    
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+            if(i==0||j==0)
+            dp[i][j]=0;
+            else if(str1[i-1]==str2[j-1])
+            dp[i][j]=1+dp[i-1][j-1];
+            else
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    cout<<dp[m][n]<<endl;
+}
