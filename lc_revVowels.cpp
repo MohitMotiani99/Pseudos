0ca5c28af41ded5518d2d2d03b55a3b2e85b8e91
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2020-09-08 14:54:16 +0530

    Create lc_revVowels.cpp

---
+++ lc_revVowels.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string v="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+                v+=s[i];
+        }
+        reverse(v.begin(),v.end());
+        int j=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+                s[i]=v[j++];
+        }
+        return s;
+    }
+};
