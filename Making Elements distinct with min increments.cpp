e8490bb8afcd70a3daaed567ece803e1bea84b3c
Mohit Motiani <34206001+MohitMotiani99@users.noreply.github.com>
2021-01-31 11:45:26 +0530

    Update Making Elements distinct with min increments.cpp

--- Making Elements distinct with min increments.cpp
+++ Making Elements distinct with min increments.cpp
@@ -4,15 +4,20 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
+    map<int,int> m;
+    for(int i=0;i<n;i++)cin>>arr[i],m[arr[i]]++;
     
-    for(int i=0;i<n;i++)cin>>arr[i];
-    
-    long sum=arr[0];
-    for(int i=1;i<n;i++){
-        if(arr[i]==arr[i-1])
-        {
-            for(int j=i;j<n && arr[j]==arr[i-1];j++)
-            arr[j]=1+arr[j-1];
+    long sum=0;
+    for(int i=n-1;i>=0;i--){
+        if(m[arr[i]]>1){
+            int orig=arr[i];
+            
+            while(m[arr[i]]>=1)
+            arr[i]++;
+            if(m[arr[i]]==0)
+            m[arr[i]]++;
+            
+            m[orig]--;
         }
         sum+=arr[i];
     }
